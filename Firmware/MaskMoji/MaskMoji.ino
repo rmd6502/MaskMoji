7c76c54ebdeb66f33dc3c08be7f484facafbe20b
Robert Diamond <rmd6502@gmail.com>
2020-09-30 20:29:03 -0700

    advertise bluetooth

--- Firmware/MaskMoji/MaskMoji.ino
+++ Firmware/MaskMoji/MaskMoji.ino
@@ -128,10 +128,11 @@ void initBLE() {
   pService->start();
 
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
-  pAdvertising->start();
+  BLEDevice::startAdvertising();
 }
 
 //====================================================================================
